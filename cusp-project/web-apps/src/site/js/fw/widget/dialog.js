8569cd556333de20e99d52908d14fb10db644f4d
vison ruan <vison_ruan@126.com>
2016-03-09 23:24:40 +0800

    update progress

---
+++ cusp-project/web-apps/src/site/js/fw/widget/dialog.js
@@ -0,0 +1,34 @@
+/**
+ * Created by vison on 2016/3/9.
+ */
+define('widget/dialog',['_g','doT'],function() {
+    var _this = this;
+
+    var builder = new function() {
+        var wconf = null;
+        var renderElem = null;
+
+
+        this.init = function(conf) {
+
+            if (!conf['renderElem']) {
+                throw Error('Property "renderElem" could not set.');
+            }
+
+            renderElem = conf['renderElem'];
+
+            if (conf['wconf']) {
+                wconf = conf['wconf'];
+            }
+        }
+
+
+        this.build = function() {
+            return renderElem.DataTable(wconf);
+        }
+
+
+    }
+
+    return builder;
+});
