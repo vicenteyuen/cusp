794518661f28a59d4ce005d629c64461455d6031
vison ruan <vison_ruan@126.com>
2016-06-09 00:32:31 +0800

    Signed-off-by: vison ruan <vison_ruan@126.com>

--- cusp-project/platform-fw/bootstrap/src/main/java/org/vsg/cusp/startup/ServerConst.java
+++
@@ -1,9 +0,0 @@
-package org.vsg.cusp.startup;
-
-public final class ServerConst {
-	
-	public static final String SERVER_FE_HOST = "server.fe.host";
-	
-	public static final String SERVER_FE_PORT = "server.fe.port";
-
-}
