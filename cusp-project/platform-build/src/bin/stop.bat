4cb7e2a2bad651d1dad361458e36a7e8ac4e29fe
vison ruan <vison_ruan@126.com>
2016-06-11 23:40:41 +0800

    udpate progress

--- cusp-project/platform-build/src/bin/stop.bat
+++ cusp-project/platform-build/src/bin/stop.bat
@@ -15,7 +15,7 @@ rem See the License for the specific language governing permissions and
 rem limitations under the License.
 
 rem ---------------------------------------------------------------------------
-rem Start script for the Custom Unify Service Server
+rem Stop script for the CATALINA Server
 rem ---------------------------------------------------------------------------
 
 setlocal
@@ -30,7 +30,6 @@ set "CUSP_HOME=%cd%"
 cd "%CURRENT_DIR%"
 
 :gotHome
-echo %CUSP_HOME%
 if exist "%CUSP_HOME%\bin\serv-run.bat" goto okHome
 echo The CUSP_HOME environment variable is not defined correctly
 echo This environment variable is needed to run this program
@@ -38,23 +37,23 @@ goto end
 
 :okHome
 set "EXECUTABLE=%CUSP_HOME%\bin\serv-run.bat"
-
 rem Check that target executable exists
 if exist "%EXECUTABLE%" goto okExec
 echo Cannot find "%EXECUTABLE%"
 echo This file is needed to run this program
 goto end
-:okExec
 
+:okExec
 rem Get remaining unshifted command line arguments and save them in the
 set CMD_LINE_ARGS=
+
 :setArgs
 if ""%1""=="""" goto doneSetArgs
 set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
 shift
 goto setArgs
-:doneSetArgs
 
-call "%EXECUTABLE%" start %CMD_LINE_ARGS%
+:doneSetArgs
+call "%EXECUTABLE%" stop %CMD_LINE_ARGS%
 
 :end
