09faf11e8f4b5a5fe66c70051c4b3ef97e5ac544
vison ruan <vison_ruan@126.com>
2016-06-13 23:35:59 +0800

    update fix bug

---
+++ cusp-project/platform-apps/product-mng/product-pub-runtime/src/main/java/org/vsg/cusp/product/pub/TestClass.java
@@ -0,0 +1,14 @@
+package org.vsg.cusp.product.pub;
+
+public class TestClass {
+
+	public TestClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- cusp-project/platform-fw/core-runtime/src/main/java/org/vsg/cusp/core/ContainerBase.java
+++ cusp-project/platform-fw/core-runtime/src/main/java/org/vsg/cusp/core/ContainerBase.java
@@ -4,6 +4,9 @@
 package org.vsg.cusp.core;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,13 +37,42 @@ public class ContainerBase {
 		// --- share folder ---
 		File shareLibFolder = new File(microCompsFolder , "share");
 		
-		// --- load the share libs ---
-		getShareClassLoader(shareLibFolder);
+		try {
+			// --- load the share libs ---
+			ClassLoader clsLoader = getShareClassLoader(shareLibFolder);
+			
+			Class cls = clsLoader.loadClass("org.vsg.cusp.product.pub.TestClass");
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
-	public ClassLoader getShareClassLoader(File shareLibFolder) {
-		return null;
+	public ClassLoader getShareClassLoader(File shareLibFolder) throws Exception {
+		
+		File[] files = shareLibFolder.listFiles(new FileFilter() {
+
+			@Override
+			public boolean accept(File pathname) {
+				// TODO Auto-generated method stub
+				String fileName = pathname.getName();
+				boolean result = fileName.endsWith(".jar");
+				return result;
+			}
+			
+		});
+		
+		URL[] jarUrls = new URL[files.length];
+		for (int i = 0 ; i < files.length ; i++) {
+			jarUrls[i] = files[i].toURI().toURL();
+
+		}
+		
+		URLClassLoader urlClsLoader = new URLClassLoader(jarUrls , this.parentClassLoader);
+
+		return urlClsLoader;
 	}
 	
 	
