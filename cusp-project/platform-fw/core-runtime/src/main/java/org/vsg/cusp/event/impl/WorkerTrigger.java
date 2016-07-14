4ddc95152ec0c94034166c750433f3153736a1aa
vison ruan <vison_ruan@126.com>
2016-07-15 06:51:18 +0800

    update progress

---
+++ cusp-project/platform-fw/core-runtime/src/main/java/org/vsg/cusp/event/impl/WorkerTrigger.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ * 
+ * 
+ */
+package org.vsg.cusp.event.impl;
+
+import org.vsg.cusp.event.Message;
+
+/**
+ * @author Vicente Yuen
+ *
+ */
+public class WorkerTrigger {
+	
+	
+	public void receiveMessage(Message msg) {
+		
+	}
+	
+	
+	public byte[] replyResponseMessage() {
+		return null;
+	}
+	
+
+}
